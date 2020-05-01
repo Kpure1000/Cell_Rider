0b651161746af66bf4def63d0f567690e226b3c7
Kpurek <ktsq2000@126.com>
2020-05-02 00:14:53 +0800

    Add Map Class.

---
+++ Assets/Script/Map_2048.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map_2048
+{
+    public Map_2048()
+    {
+        Mapper = new int[4, 4];
+        for (int i = 0; i < Mapper.GetLength(0); i++)
+        {
+            for (int j = 0; j < Mapper.GetLength(1); j++)
+            {
+                Mapper[i, j] = 0;
+            }
+        }
+
+    }
+
+
+    public int[,] Mapper;
+
+}
