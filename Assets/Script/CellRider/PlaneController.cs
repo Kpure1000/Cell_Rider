10aaa00d8c891b598ce48188882f7014ae015cb2
Kpurek <ktsq2000@126.com>
2020-05-26 00:51:51 +0800

    Commit Plane and other with Controller.

---
+++ Assets/Script/CellRider/PlaneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public float flySpeedNormal;
+
+    public float flySpeedSlow;
+
+    Vector2 moveInput;
+
+    private void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput = Vector2.ClampMagnitude(moveInput, Mathf.Sqrt(2f));
+        
+        moveInput *= Time.deltaTime * flySpeedNormal;
+
+        transform.position += new Vector3(moveInput.x, moveInput.y, transform.position.z);
+    }
+}
