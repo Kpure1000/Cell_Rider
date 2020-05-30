7eda6fd3620acca0411d17aeef3cd036bc8993d1
Kpurek <ktsq2000@126.com>
2020-05-30 11:02:11 +0800

    Time Back Control

---
+++ Assets/Script/CellRider/PlayerTimeBack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTimeBack : MonoBehaviour
+{
+    public class PlayerTimeElement
+    {
+        public Vector3 position;
+
+        public Sprite sprite;
+
+        public PlayerTimeElement(Vector3 pos, Sprite spr)
+        {
+            position = pos;
+            sprite = spr;
+        }
+    }
+
+    TimeStack<PlayerTimeElement> timeStack;
+
+    public bool isBacking;
+
+    /**********************************************************/
+    //外部依赖
+    [Tooltip("依赖的Sprite所在Renderer")]
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
+
+    public int TimeBackLength;
+
+    /**********************************************************/
+
+    private void Start()
+    {
+        isBacking = false;
+        timeStack = new TimeStack<PlayerTimeElement>(TimeBackLength);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isBacking = true;
+
+            if (timeStack.isEmpty() == false)
+            {
+                transform.position = timeStack.Top().position;
+                spriteRenderer.sprite = timeStack.Top().sprite;
+                timeStack.Pop();
+            }
+        }
+        else
+        {
+            isBacking = false;
+
+            timeStack.Push(new PlayerTimeElement(transform.position, spriteRenderer.sprite));
+        }
+
+        if (isBacking)
+        {
+            animator.enabled = false;
+        }
+        else
+        {
+            animator.enabled = true;
+        }
+
+    }
+    
+}
---
+++ Assets/Script/CellRider/TimeStack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+/// <summary>
+/// 时间回溯容器(支持出栈的循环队列)
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class TimeStack<T>
+{
+    private T[] _array;
+
+    private int _front;
+
+    private int _rear;
+
+    public int MaxSize;
+
+    public TimeStack(int maxSize)
+    {
+        _array = new T[maxSize];
+        MaxSize = maxSize;
+
+        _front = _rear = 0;
+    }
+    /// <summary>
+    /// 压栈，覆盖
+    /// </summary>
+    /// <param name="value"></param>
+    public void Push(T value)
+    {
+        //Debug.Log(string.Format("Fonrt: {0}, Rear: {1}", _front, _rear));
+
+        _front = (_front + 1) % MaxSize;
+
+        //首尾相接
+        if (_front == _rear)
+        {
+            _rear = (_front + 1) % MaxSize;
+        }
+
+        _array[_front] = value;
+    }
+    /// <summary>
+    /// 安全的出栈方法
+    /// </summary>
+    public void Pop()
+    {
+        if (isEmpty()) 
+            return;
+
+        _front = (_front + MaxSize - 1) % MaxSize;
+
+    }
+    /// <summary>
+    /// 不安全的栈顶，最好先判断isEmpty
+    /// </summary>
+    /// <returns></returns>
+    public T Top()
+    {
+        if (isEmpty() == false)
+        {
+            return _array[_front];
+        }
+        return default;
+    }
+    public bool isFull()
+    {
+        return (_front + 1) % MaxSize == _rear;
+    }
+    /// <summary>
+    /// 是否为空
+    /// </summary>
+    /// <returns></returns>
+    public bool isEmpty()
+    {
+        return _front == _rear;
+    }
+}
